75ea22356013d177fa395e4024286e90075aa7d8
Elizabeth <wuhanda12@yahoo.com>
2020-01-22 16:11:09 -0500

    redirects to dashboard after login

--- frontend/src/containers/LoginPage/index.js
+++ frontend/src/containers/LoginPage/index.js
@@ -1,43 +1,51 @@
+/* eslint-disable linebreak-style */
 import React, { useState } from 'react'
-import { Link, Container, LoginContainer, Input, Title, Font } from './styles'
-import { SignupButton } from '../Home/styles'
 import { useMutation, useLazyQuery } from '@apollo/react-hooks'
-import { LOGIN, TESTQUERY} from './graphql.js'
+import { useHistory } from 'react-router-dom'
+import {
+  Link, Container, LoginContainer, Input, Title, Font,
+} from './styles'
+import { SignupButton } from '../Home/styles'
+import { LOGIN, TESTQUERY } from './graphql.js'
 
 const Login = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [login] = useMutation(LOGIN, {
-        variables: {
-            email,
-            password,
-        },
-        onCompleted: ({ login: { token } }) => localStorage.setItem('token', token), 
-    })
-    const [searchPrice] = useLazyQuery(TESTQUERY, {
-        variables: {
-            "input": { "high": 1000, "low": 150 }
-        }
-    })
+  const history = useHistory()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [login, { error, loading }] = useMutation(LOGIN, {
+    variables: {
+      email,
+      password,
+    },
+    onCompleted: ({ login: { token } }) => {
+      localStorage.setItem('token', token)
+      history.push('/dashboard')
+    },
+  })
+  const [searchPrice] = useLazyQuery(TESTQUERY, {
+    variables: {
+      input: { high: 1000, low: 150 },
+    },
+  })
 
-    return (
-        <Container>  
-            <Font>
-            <LoginContainer>
-                <Title>Login</Title>
-                <Input placeholder="email" value={email} onChange={e => setEmail(e.target.value)} />
-                <Input placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
-                <button type="button" onClick={login}> Login </button>
-                <button type="button" onClick={searchPrice}>Welcome!</button>
-                <p>Need to make an account?</p>
-                <Link to="/register">
-                    <SignupButton>Signup</SignupButton>
-                </Link>
-            </LoginContainer>
-            </Font> 
-        </Container>
+  return (
+    <Container>
+      <Font>
+        <LoginContainer>
+          <Title>Login</Title>
+          <Input placeholder="email" value={email} onChange={e => setEmail(e.target.value)} />
+          <Input placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <button type="button" onClick={login}> Login </button>
+          <button type="button" onClick={searchPrice}>Welcome!</button>
+          <p>Need to make an account?</p>
+          <Link to="/register">
+            <SignupButton>Signup</SignupButton>
+          </Link>
+        </LoginContainer>
+      </Font>
+    </Container>
 
-    )
+  )
 }
 
 export default Login
